cf7dc98e708dc88b8952e13806eb1259bdb58fd9
kryptocodes <srivatsantb@gmail.com>
2024-07-07 23:55:18 +0530

    feat: add leadboard routes
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { Router, json } from 'express';
 import { getUser } from '../controllers/userController';
+import { getRefferalCounts, getUserAndPoints } from '../controllers/leaderboard'
 import { isSignedIn } from '../middleware/auth';
 
 const router = Router();
@@ -7,6 +8,7 @@ const router = Router();
 // router.post('/register', register);
 router.use(json());
 router.get('/',isSignedIn,getUser);
-
+router.get('/leaderboard', getUserAndPoints);
+router.get('/leaderboard/referral', getRefferalCounts);
 
 export default router;
